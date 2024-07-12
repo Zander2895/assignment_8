a14a1456a169995f8463ead40d5ac2ff4586913c
Zander2895 <jacquesalexander71@gmail.com>
2024-07-11 22:32:06 -0400

    first commit

---
+++ dictionary_applications.py
@@ -0,0 +1,21 @@
+# Task 1: Restaurant Menu Update You are given an initial structure of a restaurant menu stored in a nested dictionary. Your task is to update this menu based on given instructions
+
+# Problem Statement: Given the initial menu:
+
+restaurant_menu = {
+    "Starters": {"Soup": 5.99, "Bruschetta": 6.50},
+    "Main Course": {"Steak": 15.99, "Salmon": 13.99},
+    "Desserts": {"Cake": 4.99, "Ice Cream": 3.99}
+}
+
+# - Add a new category called "Beverages" with at least two items.
+restaurant_menu["Beverages"] = {"Coke": 2.25, "Dr. Peper": 2.00}
+print(restaurant_menu)
+
+# - Update the price of "Steak" to 17.99.
+restaurant_menu["Main Course"]["Steak"] = 17.99
+print(restaurant_menu)
+
+# - Remove "Bruschetta" from "Starters". 
+restaurant_menu["Starters"].pop("Bruschetta", 6.50)
+print(restaurant_menu)
